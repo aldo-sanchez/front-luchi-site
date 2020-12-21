ebca17e0d9eca6d96d3e1815462781f55291e18c
Aldo Sanchez <aldo@innowatts.com>
2020-12-21 16:59:19 -0600

    Adds initial data fetch to cms

--- front-luchi-site/pages/about.js
+++ front-luchi-site/pages/about.js
@@ -1,6 +1,30 @@
-const About = () => {
+import ReactMarkdown from 'react-markdown'
+
+export async function getStaticProps() {
+  const url = new URL('/abouts', process.env.CMS_HOST).href
+  const res = await fetch(url)
+  const data = await res.json()
+  if (!data || data.length === 0) {
+    return {
+      notFound: true
+    }
+  }
+  return {
+    props: {
+      data: data[0]
+    },
+  }
+}
+
+const About = (props) => {
+  console.log('props?', props)
   return (
-    <h1>Welcome to the about machine</h1>
+    <div>
+      <h1>Welcome to the about machine</h1>
+      <ReactMarkdown>
+        {props.data.description}
+      </ReactMarkdown>
+    </div>
   )
 }
 
