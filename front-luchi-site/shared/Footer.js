67397fbc9c271a53c96c2e8876ef9e961bb5f20f
Aldo Sanchez <aldo@innowatts.com>
2021-01-03 11:47:17 -0600

    Cleans footer

--- front-luchi-site/shared/Footer.js
+++ front-luchi-site/shared/Footer.js
@@ -3,16 +3,24 @@ import { Instagram, Linkedin, Youtube } from 'react-feather'
 const Footer = () => {
   return (
     <footer>
-      <span className="copyright">&copy; {new Date().getFullYear()}</span>
-      <a href="https://instagram.com" target="_blank">
-        <Instagram />
-      </a>
-      <a href="https://linkedin.com" target="_blank">
-        <Linkedin />
-      </a>
-      <a href="https://youtube.com" target="_blank">
-        <Youtube />
-      </a>
+      <small className="copyright">&copy; {new Date().getFullYear()}</small>
+      <span>
+        <small>
+          <a href="https://instagram.com" target="_blank">
+            <Instagram />
+          </a>
+        </small>
+        <small>
+          <a href="https://linkedin.com" target="_blank">
+            <Linkedin />
+          </a>
+        </small>
+        <small>
+          <a href="https://youtube.com" target="_blank">
+            <Youtube />
+          </a>
+        </small>
+      </span>
     </footer>
   )
 }
